d3d119861df795ffd1296a13b42a8678f6b51f6d
CHRIS JACKSON <chrisjackson@chriss-mbp.home>
2020-01-31 23:19:18 +0000

    port change

--- elb_test_us_east_aws.tf
+++ elb_test_us_east_aws.tf
@@ -87,8 +87,8 @@ resource "aws_security_group" "elb_sg" {
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 443 
+    to_port     = 443 
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
